3d50fd504d8656ab740c6562c1e453d1166a886a
taivo2209 <taivo1213@gmail.com>
2023-02-26 16:29:09 +0700

    commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography, IconButton, Stack, Button } from '@mui/material'
 import HouseIcon from '@mui/icons-material/House';
+import './style.css'
 
 function Navbar() {
   return (
-    <div>
-        <AppBar position="static" color="primary">
+    <div style={{
+      display: 'flex',
+      justifyContent:'center'
+    }}>
+        <AppBar position="static" color="transparent" className='navbar'>
           <Toolbar>
             <IconButton size='large' edge='start' color='inherit' aria-label="logo" >
               <HouseIcon/>
@@ -13,11 +17,11 @@ function Navbar() {
             <Typography variant="h6" component={'h6'} sx={{flexGrow: 1}}>
               Bee Home
             </Typography>
-            <Stack direction={'row'} spacing={5}>
-                <Button color="inherit">Aplus HOME</Button>
-                <Button color="inherit">Aplus SHARE</Button>
-                <Button color="inherit">LOGIN</Button>
-                <Button color="inherit">FOR HOUSE OWNER</Button>
+            <Stack direction={'row'} spacing={2}>
+                <Button color="inherit" className='button-1'>Aplus HOME</Button>
+                <Button color="inherit" className='button-1'>Aplus SHARE</Button>
+                <Button color="inherit" className='button-2'>LOGIN</Button>
+                <Button color="inherit" className='button-2'>FOR HOUSE OWNER</Button>
             </Stack>
           </Toolbar>
         </AppBar>
